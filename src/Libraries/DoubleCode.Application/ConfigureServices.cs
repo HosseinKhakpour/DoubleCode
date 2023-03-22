57f09a28301585803b58485302d331683dd19e16
HosseinKhakpour <Khakpwr2@gmail.com>
2023-03-22 12:35:59 +0430

    Add mediaror

---
+++ src/Libraries/DoubleCode.Application/ConfigureServices.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace DoubleCode.Application;
+public static class ConfigureServices
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+
+        // Add any other services needed for the application
+
+        return services;
+    }
+}
+
--- src/Presentation/DoubleCode.WebUI/Program.cs
+++ src/Presentation/DoubleCode.WebUI/Program.cs
@@ -1,25 +1,10 @@
-var builder = WebApplication.CreateBuilder(args);
+using DoubleCode.Application;
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddApplicationServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
