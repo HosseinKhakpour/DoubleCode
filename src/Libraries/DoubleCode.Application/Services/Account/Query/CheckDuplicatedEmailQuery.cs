3bbd5e08b4439ffd1527071264cab1ecc4aa96ae
h.khakpour <h.khakpour@LPNJR-CTM-RDS>
2023-05-15 11:31:56 +0330

    Update

--- src/Libraries/DoubleCode.Application/Services/Account/Query/CheckDuplicatedEmailQuery.cs
+++ src/Libraries/DoubleCode.Application/Services/Account/Query/CheckDuplicatedEmailQuery.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using DoubleCode.Application.Common.Interfaces;
+using DoubleCode.Domain.Base;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DoubleCode.Application.Services.Account.Query
+namespace DoubleCode.Application.Services.Account.Query;
+
+public class CheckDuplicatedEmailQuery : IRequest<BaseResult_VM<bool>>
 {
-    internal class CheckDuplicatedEmailQuery
-    {
-    }
+    public string Email { get; set; }
 }
+public class CheckDuplicatedEmailQueryHandler : IRequestHandler<CheckDuplicatedEmailQuery, BaseResult_VM<bool>>
+{
+    #region Property
+    private readonly IApplicationDbContext _context;
+    #endregion
+
+    #region Ctor
+    public CheckDuplicatedEmailQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+    #endregion
+
+    #region Method
+    public async Task<BaseResult_VM<bool>> Handle(CheckDuplicatedEmailQuery request, CancellationToken cancellationToken)
+    {
+        bool isDuplicated = await _context.User.AnyAsync(u => u.Email ==request.Email);
+        if (isDuplicated) 
+        {
+            return new BaseResult_VM<bool>
+            {
+                Result = true,
+                Code =0,
+                Message ="",
+            };
+        }
+
+        return new BaseResult_VM<bool>
+        {
+            Result = false,
+            Code =-1,
+            Message ="کاربر با این ایمیل وجود دارد ",
+        };
+    }
+
+    #endregion
+}
+
+
--- src/Libraries/DoubleCode.Application/Services/Account/Query/CheckDuplicatedUserNameQuery.cs
+++ src/Libraries/DoubleCode.Application/Services/Account/Query/CheckDuplicatedUserNameQuery.cs
@@ -1,12 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DoubleCode.Application.Common.Interfaces;
+using DoubleCode.Domain.Base;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
 
-namespace DoubleCode.Application.Services.Account.Query
+namespace DoubleCode.Application.Services.Account.Query;
+
+public class CheckDuplicatedUserNameQuery : IRequest<BaseResult_VM<bool>>
 {
-    internal class CheckDuplicatedUserNameQuery
-    {
-    }
+    public string UserName { get; set; }
 }
+public class CheckDuplicatedUserNameQueryHandler : IRequestHandler<CheckDuplicatedUserNameQuery, BaseResult_VM<bool>>
+{
+    #region Property
+    private readonly IApplicationDbContext _context;
+    #endregion
+
+    #region Ctor
+    public CheckDuplicatedUserNameQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+    #endregion
+
+    #region Method
+    public async Task<BaseResult_VM<bool>> Handle(CheckDuplicatedUserNameQuery request, CancellationToken cancellationToken)
+    {
+        bool isDuplicated = await _context.User.AnyAsync(u => u.UserName ==request.UserName);
+        if (isDuplicated)
+        {
+            return new BaseResult_VM<bool>
+            {
+                Result = true,
+                Code =0,
+                Message ="",
+            };
+        }
+
+        return new BaseResult_VM<bool>
+        {
+            Result = false,
+            Code =-1,
+            Message ="این نام کاربری تکراری است",
+        };
+    }
+
+    #endregion
+}
+
+
